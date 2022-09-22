48392cf756e10343a58fd7dbc4065b13619e18b8
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2022-09-22 23:56:57 +0900

    11

--- my-app/src/home/Home.js
+++ my-app/src/home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const Home = () => {
+// function 방식
+// class 방식
+const Home = (props) => {
+  //구조분할 할당
+  const { boards, setBoards } = props;
   return (
     <div>
       <div>
         <h1>홈페이지입니다</h1>
+        <button onClick={() => setBoards([])}>전체삭제</button>
+        {boards.map((board) => (
+          <h3>
+            제목:{board.title} 내용:{board.content}
+          </h3>
+        ))}
       </div>
     </div>
   );
--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Home from '../home/Home';
 
 const HomePage = () => {
+  //http요청 (fetch, axios(다운))
+  const [boards, setBoards] = useState([]); //boards는 상태데이터여야 한다. 그래야 데이터가 넘어왔을때 상태가 변함?
+
+  //빈 배열 한번만 실행
+  useEffect(() => {
+    //다운로드 가정=fetch(),axios(),ajax()
+    let data = [
+      { id: 1, title: '제목1', content: '내용1' },
+      { id: 2, title: '제목2', content: '내용2' },
+      { id: 3, title: '제목3', content: '내용3' },
+    ];
+
+    // 빈데이터
+    setBoards([...data]);
+  }, []);
   return (
     <div>
       <Header />
-      <Home />
+      <Home boards={boards} setBoards={setBoards} />
       <Footer />
     </div>
   );
